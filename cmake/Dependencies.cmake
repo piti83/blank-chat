9a0ebca535114a07daf3ad24a9d084002df19cdc
Piotr Walczak <piotr.t.walczak@proton.me>
2026-04-23 23:59:59 +0200

    Fix sodium issues

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -18,24 +18,25 @@ if(USE_FETCH_CONTENT)
 
   set(LIBSODIUM_PREFIX "${CMAKE_BINARY_DIR}/external/libsodium")
   set(LIBSODIUM_INCLUDE_DIR "${LIBSODIUM_PREFIX}/include")
-  set(LIBSODIUM_LIB_DIR "${LIBSODIUM_PREFIX}/lib")
-  set(LIBSODIUM_STATIC_LIB "${LIBSODIUM_LIB_DIR}/libsodium.a")
+  set(LIBSODIUM_LIB "${LIBSODIUM_PREFIX}/lib/libsodium.a")
 
   ExternalProject_Add(libsodium_external
       GIT_REPOSITORY      https://github.com/jedisct1/libsodium.git
       GIT_TAG             1.0.19-RELEASE
       PREFIX              ${LIBSODIUM_PREFIX}
       CONFIGURE_COMMAND   <SOURCE_DIR>/configure --prefix=${LIBSODIUM_PREFIX} --enable-static --disable-shared --with-pic
-      BUILD_COMMAND       make -j$(nproc)
+
+      BUILD_COMMAND       make -j4
+
       INSTALL_COMMAND     make install
       UPDATE_COMMAND      ""
-      BUILD_BYPRODUCTS    ${LIBSODIUM_STATIC_LIB}
+      BUILD_BYPRODUCTS    ${LIBSODIUM_LIB}
   )
 
   add_library(sodium_static STATIC IMPORTED GLOBAL)
   add_dependencies(sodium_static libsodium_external)
   set_target_properties(sodium_static PROPERTIES
-      IMPORTED_LOCATION "${LIBSODIUM_STATIC_LIB}"
+      IMPORTED_LOCATION "${LIBSODIUM_LIB}"
       INTERFACE_INCLUDE_DIRECTORIES "${LIBSODIUM_INCLUDE_DIR}"
   )
 endif()
