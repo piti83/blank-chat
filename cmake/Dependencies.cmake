2eeeaca8e2d565004e967712b515717d42064ae6
piti83 <106607310+piti83@users.noreply.github.com>
2026-04-27 19:27:45 +0200

    Update recipes and cmake dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,16 @@
+find_package(PkgConfig REQUIRED)
+
 add_library(ext_sodium INTERFACE)
 add_library(ext::sodium ALIAS ext_sodium)
-
-find_package(PkgConfig REQUIRED)
 pkg_check_modules(SODIUM REQUIRED libsodium)
 target_link_libraries(ext_sodium INTERFACE ${SODIUM_LIBRARIES})
 target_include_directories(ext_sodium INTERFACE ${SODIUM_INCLUDE_DIRS})
+
+find_package(Boost REQUIRED COMPONENTS system)
+add_library(ext_boost INTERFACE)
+add_library(ext::boost ALIAS ext_boost)
+target_include_directories(ext_boost INTERFACE ${Boost_INCLUDE_DIRS})
+target_link_libraries(ext_boost INTERFACE ${Boost_LIBRARIES})
 
 find_package(spdlog CONFIG REQUIRED)
 find_package(GTest CONFIG REQUIRED)
