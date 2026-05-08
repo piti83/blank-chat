e46c8cd4586effeaec5ec6e953fcd41c4fb2e9b5
piti83 <106607310+piti83@users.noreply.github.com>
2026-05-08 17:43:16 +0200

    Temp commit

--- libs/core/src/secure_buffer.cpp
+++ libs/core/src/secure_buffer.cpp
@@ -1,15 +1,47 @@
 #include <core/secure_buffer.h>
-#include <cstdint>
+#include <utility>
 
 namespace bc::core {
 
 SecureBuffer::SecureBuffer(std::size_t size) : bufferSize(size)
 {
     if (size > 0) {
+        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
         bufferData = new BufferType[size]();
     } else {
         bufferData = nullptr;
     }
+}
+
+SecureBuffer::~SecureBuffer() noexcept
+{
+    delete[] bufferData;
+}
+
+SecureBuffer::SecureBuffer(SecureBuffer&& other) noexcept
+{
+    bufferSize = std::exchange(other.bufferSize, 0);
+    bufferData = std::exchange(other.bufferData, nullptr);
+}
+
+auto SecureBuffer::operator=(SecureBuffer&& other) noexcept -> SecureBuffer&
+{
+    if (this != &other) {
+        delete[] this->bufferData;
+        this->bufferSize = std::exchange(other.bufferSize, 0);
+        this->bufferData = std::exchange(other.bufferData, nullptr);
+    }
+    return *this;
+}
+
+auto SecureBuffer::Data() noexcept -> BufferType*
+{
+    return bufferData;
+}
+
+auto SecureBuffer::Size() const noexcept -> std::size_t
+{
+    return bufferSize;
 }
 
 } // namespace bc::core
