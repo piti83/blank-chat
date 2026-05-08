f5a6b1cdbb1e1a234aa20a7e43e86bcc9b097085
piti83 <106607310+piti83@users.noreply.github.com>
2026-05-08 19:31:50 +0200

    Implement SecureBuffer (#36)

---
+++ libs/core/src/secure_buffer.cpp
@@ -0,0 +1,67 @@
+#include <core/secure_buffer.h>
+#include <utility>
+
+namespace bc::core {
+
+SecureBuffer::SecureBuffer(std::size_t size) : bufferSize(size)
+{
+    if (size > 0) {
+        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
+        bufferData = new BufferType[size]();
+    } else {
+        bufferData = nullptr;
+    }
+}
+
+SecureBuffer::~SecureBuffer() noexcept
+{
+    delete[] bufferData;
+}
+
+SecureBuffer::SecureBuffer(SecureBuffer&& other) noexcept
+{
+    bufferSize = std::exchange(other.bufferSize, 0);
+    bufferData = std::exchange(other.bufferData, nullptr);
+}
+
+auto SecureBuffer::operator=(SecureBuffer&& other) noexcept -> SecureBuffer&
+{
+    if (this != &other) {
+        delete[] this->bufferData;
+        this->bufferSize = std::exchange(other.bufferSize, 0);
+        this->bufferData = std::exchange(other.bufferData, nullptr);
+    }
+    return *this;
+}
+
+auto SecureBuffer::Data() noexcept -> BufferType*
+{
+    return bufferData;
+}
+
+auto SecureBuffer::Data() const noexcept -> const BufferType*
+{
+    return bufferData;
+}
+
+auto SecureBuffer::Size() const noexcept -> std::size_t
+{
+    return bufferSize;
+}
+
+auto SecureBuffer::IsEmpty() const noexcept -> bool
+{
+    return bufferSize == 0;
+}
+
+auto SecureBuffer::AsSpan() const noexcept -> std::span<const BufferType>
+{
+    return {bufferData, bufferSize};
+}
+
+auto SecureBuffer::AsMutableSpan() noexcept -> std::span<BufferType>
+{
+    return {bufferData, bufferSize};
+}
+
+} // namespace bc::core
