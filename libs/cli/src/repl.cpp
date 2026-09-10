decae89d38fdb89759099a13949da371a07b908e
piti83 <106607310+piti83@users.noreply.github.com>
2026-09-10 02:09:32 +0200

    [PATCH] Fix format

--- libs/cli/src/repl.cpp
+++ libs/cli/src/repl.cpp
@@ -644,7 +644,8 @@ auto Repl::ProcessAckFrame(std::string_view alias, bc::domain::client::Contact* 
     sodium_memzero(plaintextOpt->data(), plaintextOpt->size());
 }
 
-auto Repl::InitiatePfsRotation([[maybe_unused]]std::string_view alias, bc::domain::client::Contact* contact) -> bool
+auto Repl::InitiatePfsRotation([[maybe_unused]] std::string_view alias,
+                               bc::domain::client::Contact* contact) -> bool
 {
     if (contact == nullptr || contact->pfsState != bc::domain::client::PfsState::IDLE) {
         return false;
